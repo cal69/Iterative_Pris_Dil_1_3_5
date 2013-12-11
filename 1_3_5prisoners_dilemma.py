22ad504abec8916486e4e069ddeb624193fe5e89
CalJurgella <caljurgella@gmail.com>
2013-12-11 13:06:49 -0600

    number 17

--- 1_3_5prisoners_dilemma.py
+++ 1_3_5prisoners_dilemma.py
@@ -525,14 +525,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 17:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'idiot'
         else:
-            if len(opponent_history)==0: #It's the first round: collude
+            if len(opponent_history)==0:
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            elif opponent_history[len(opponent_history)-1] == 'b':
+                return 'b'
+            elif history[len(history)-1]=='c' and opponent_history[len(opponent_history)-1]=='b':
+                return 'b' # betray if they were severely punished last time
             else:
-                return 'c' #otherwise collude
+                return 'c'
     
     
 
